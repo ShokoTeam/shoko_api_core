62d946f543e5cbac32cb12b000747385a585f67e
shoshideveloper <lobachev-2019@mail.ru>
2025-02-09 23:25:34 +0500

    feat(core): add source model

--- shoko_api_core/lib/src/_internal.dart
+++ shoko_api_core/lib/src/_internal.dart
@@ -7,5 +7,6 @@ export 'domain/utils/error_code.dart';
 export 'domain/utils/logger.dart';
 
 export 'domain/models/route_controller.dart';
+export 'domain/models/source.dart';
 
 export 'config.dart';
---
+++ shoko_api_core/lib/src/domain/models/source.dart
@@ -0,0 +1,3 @@
+abstract interface class Source<T> {
+  abstract T db;
+}

31a2eb22b9a0dfc80e2c8360b421c8edaefb98d2
P.Lobachev <pavel.lobachev@finbeat.tech>
2025-10-04 17:15:16 +0500

    fix: example fixes

--- exmaple_api/main.dart
+++ exmaple_api/main.dart
@@ -3,7 +3,7 @@ import 'dart:io';
 import 'package:shoko_api_core/shoko_api_core.dart';
 
 Future<void> init(InternetAddress ip, int port) async {
-  Config.initialize('pG9BZ4slhFjE1nTmin6C5WSvBbjuCyJk', false);
+  // Config.initialize('pG9BZ4slhFjE1nTmin6C5WSvBbjuCyJk', false);
 }
 
 Future<HttpServer> run(Handler handler, InternetAddress ip, int port) {
--- exmaple_api/routes/cars.dart
+++ exmaple_api/routes/cars.dart
@@ -1,15 +1,11 @@
 import 'package:shoko_api_core/shoko_api_core.dart';
 
-Future<Response> onRequest(RequestContext context) async => _RouteController().direct(context.request);
+Future<Response> onRequest(RequestContext context) async =>
+    _RouteController().direct(context.request);
 
 class _RouteController extends RouteController {
-  @override
-  List<RouteDocs>? get docs => [RouteDocs(HttpMethod.get)];
-
   @override
   Future<Response> get(Request request, [Map<String, dynamic>? dynamicRouteParams]) {
     return super.get(request, dynamicRouteParams);
   }
 }
-
-class RequestT {}
